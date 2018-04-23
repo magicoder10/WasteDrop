8e6cea4d5667fbef29fb7b2d8936229c8d5524b0
Harry Liu <byliuyang11@gmail.com>
2018-04-23 16:43:21 -0400

    Add leader board APIs

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const fs = require('fs');
+
+const leaderBoardDataFilename = 'data/leader-board.data.json';
+
+function getTopFivePlayers(scoreRows) {
+    scoreRows.sort((row1, row2) => row2.score - row1.score);
+    return scoreRows.slice(0, 5);
+}
+
+router.get('/leader-board', function (req, res, next) {
+    fs.readFile(leaderBoardDataFilename, 'utf8', (error, data) => {
+        if(error) {
+            res.end(error);
+            return
+        }
+
+        let scoreRows = JSON.parse(data);
+        res.end(JSON.stringify(getTopFivePlayers(scoreRows)));
+    })
+});
+
+router.post('/leader-board', function (req, res, next) {
+    let secret = req.get('Authorization');
+    if(secret !== process.env.SECRET) {
+        res.status(401);
+        res.end();
+        return;
+    }
+    fs.readFile(leaderBoardDataFilename, 'utf8', (error, data) => {
+        if(error) {
+            res.end(error);
+            return
+        }
+
+        let scoreRows = JSON.parse(data);
+        scoreRows.append(JSON.parse(req.body));
+
+        fs.writeFile(leaderBoardDataFilename, JSON.stringify(scoreRows), 'utf8', error => {
+            if(error) {
+                res.end(error);
+                return
+            }
+            res.end(JSON.stringify(getTopFivePlayers(scoreRows)));
+        });
+    })
 });
 
 module.exports = router;
